151ec604525320224782aa54f6616f111164b016
Rodrigo Araujo <alf.rodrigo@gmail.com>
2020-06-03 17:10:08 -0300

    Adding a new themes

--- assets/img/themes/preprocess/generate_schemes.py
+++ assets/img/themes/preprocess/generate_schemes.py
@@ -22,16 +22,29 @@ for img_name in all_img:
     base_colors = 3
     if len(theme_name) > 6:
         base_colors = 4
+    if theme_name == "mike_was_right" or theme_name == "OrderedPastel":
+        base_colors = 0
     img = Image.open(img_name)
     pix = img.load()
     x = 45
     scheme = f"charty://add-theme?name={theme_name}&baseColors={base_colors}&colors="
+    last_color = ""
+    count = 0
     for i in range(10):
         color = pix[x,75]
         x += 90
-        scheme += rgb2hex(color[0], color[1], color[2])
-        if i != 9:
-            scheme += ","
+        new_color = rgb2hex(color[0], color[1], color[2])
+        if new_color != last_color:
+            count += 1
+            scheme += new_color
+            if i != 9:
+                scheme += ","
+        last_color = new_color
+    if scheme[-1] == ",":
+        scheme = scheme[:-1]
+    if count == 5:
+        scheme = scheme.replace("baseColors=3", "baseColors=0")
+        scheme = scheme.replace("baseColors=4", "baseColors=0")
     new_column = base_str.replace("$scheme", scheme)
     new_column = new_column.replace("$theme_name", theme_name)
     new_column = new_column.replace("$img_name", img_name)
